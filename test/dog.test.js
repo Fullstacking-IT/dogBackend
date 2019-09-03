9da17662d8580e450430b4957dd1887d89baae74
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-03 10:18:46 -0700

    created my route to get all dogs and have a passing test

--- test/dog.test.js
+++ test/dog.test.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
+const Dog = require('../lib/models/Dog');
 
 describe('dog routes', () => {
   beforeAll(() => {
@@ -23,16 +24,39 @@ describe('dog routes', () => {
       .send({ 
         name: 'Pennie', 
         breed: 'Doodle',
-        age: '7lbs'
+        age: '7 lbs'
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           name: 'Pennie', 
           breed: 'Doodle',
-          age: '7lbs',
+          age: '7 lbs',
           __v: expect.any(Number)
         });
       });
   });
+
+  it('can get all dogs', async() => {
+    const dog = await Dog.create([{
+      name: 'Pennie', 
+      breed: 'Doodle',
+      age: '7 lbs'
+    },
+    {
+      name: 'Poochie', 
+      breed: 'Bloodhound',
+      age: '120 lbs'
+    }]);
+
+    return request(app)
+      .get('/api/v1/dogs')
+      .then(res => {
+        const dogsJSON = JSON.parse(JSON.stringify(dog));
+        dogsJSON.forEach(dog => {
+          expect(res.body).toContainEqual({ name: dog.name, breed: dog.breed, age: dog.age, _id: dog._id });
+        });
+      });
+  });
+
 });
